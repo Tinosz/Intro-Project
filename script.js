72c1559601f50fd883ffdb01f80a23721f41a725
Tinosz <valentinomahardhikaputra@gmail.com>
2023-04-02 00:37:15 +0700

    add Hunger Bar function

--- script.js
+++ script.js
@@ -3,51 +3,71 @@ const progressBar2 = document.getElementById('food');
 const progressBar3 = document.getElementById('sleep');
 const progressBar4 = document.getElementById('health');
 
+
 //energy bar
 let interval1 = setInterval(() => {
   let progressValue = parseFloat(progressBar1.style.width) || 100.0;
   progressValue -= 0.1;
   progressBar1.style.width = `${progressValue}%`;
-
+  
   if (progressValue <= 20.0) {
     progressBar1.style.backgroundColor = 'red';
   }
-
+  
   if (progressValue <= 0) {
     clearInterval(interval1);
   }
 }, 1000);
 
 //hunger bar
+let decreasingHunger= true; //membuat flag biar tahu kalau sedang berkurang atau enggak 
 let interval2 = setInterval(() => {
   let progressValue = parseFloat(progressBar2.style.width) || 100.0;
-  progressValue -= 0.1;
-  progressBar2.style.width = `${progressValue}%`;
+  if(decreasingHunger){
+    progressValue -= 0.1;
+    progressBar2.style.width = `${progressValue}%`;
+  }
 
   if (progressValue <= 20.0) {
     progressBar2.style.backgroundColor = 'red';
   }
 
   if (progressValue <= 0) {
-    clearInterval(interval2);
+    decreasingHunger= false;
     alert("I'm feeling hungry :(");
   }
-}, 7);
+}, 100); //range barnya
+
+let eatButton = document.getElementById("eatButton");
+eatButton.addEventListener("click", () =>{
+  decreasingHunger= true;
+  let currentHungerWidth = parseFloat(progressBar2.style.width);
+  let newHungerWidth = currentHungerWidth + 10; //seberapa besar ingin menambahkan makannya, make persentase
+  if (newHungerWidth > 100){
+    newHungerWidth = 100; 
+  }
+  progressBar2.style.width = `${newHungerWidth}%`;
+  progressBar2.style.backgroundColor = 'green';
+});
 
 //fatigue bar
+let decreasingSleep = true;
 let interval3 = setInterval(() => {
-  let progressValue = parseFloat(progressBar3.style.width) || 100.0;
-  progressValue -= 0.1;
-  progressBar3.style.width = `${progressValue}%`;
+  if(decreasingSleep){
+    let progressValue = parseFloat(progressBar3.style.width) || 100.0;
+    progressValue -= 0.1;
+    progressBar3.style.width = `${progressValue}%`;
+  }
 
   if (progressValue <= 20.0) {
     progressBar3.style.backgroundColor = 'red';
   }
 
   if (progressValue <= 0) {
-    clearInterval(interval3);
+    decreasingSleep = false;
+    alert("I'm feeling sleepy zzz");
   }
-}, 2000);
+}, 7);
 
 const img = document.getElementById('myImg');
 let currentImgIdx = 0;
