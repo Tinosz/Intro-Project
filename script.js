32a9515d717391b27ee7791a7937412f8cbeb5e0
Stevenleez <stevenaxel2003@gmail.com>
2023-04-02 13:21:59 +0700

    123

--- script.js
+++ script.js
@@ -101,9 +101,11 @@ function startTime() {
   var today = new Date();
   var h = today.getHours();
   var m = today.getMinutes();
+  var s = today.getSeconds();
   m = checkTime(m);
+  s = checkTime(s);
   document.getElementById('time').innerHTML =
-  h + ":" + m;
+  h + ":" + m + ":" + s;
   var t = setTimeout(startTime, 500);
 }
 
